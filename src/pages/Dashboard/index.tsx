0708ad4dc4b487f612ba256ba64d9ff610f4adb1
Ian Bezerra de Mesquita <ian.mesquita@inep.gov.br>
2020-08-31 23:20:32 -0300

    Creating dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Form, Repositories, Title } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/29241472?s=460&u=1f7adc75ce252f58f35f83f7fabbd9047cdd2a00&v=4"
+            alt="Ian Mesquita"
+          />
+          <div>
+            <strong>ianbmesquita</strong>
+            <p>Desenvolvedor Web desde 2015.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/29241472?s=460&u=1f7adc75ce252f58f35f83f7fabbd9047cdd2a00&v=4"
+            alt="Ian Mesquita"
+          />
+          <div>
+            <strong>ianbmesquita</strong>
+            <p>Desenvolvedor Web desde 2015.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/29241472?s=460&u=1f7adc75ce252f58f35f83f7fabbd9047cdd2a00&v=4"
+            alt="Ian Mesquita"
+          />
+          <div>
+            <strong>ianbmesquita</strong>
+            <p>Desenvolvedor Web desde 2015.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
